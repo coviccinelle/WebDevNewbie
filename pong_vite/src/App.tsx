a7def0f208b0d7396b833a2c5bca71dd38f71de4
coviccinelle_mac <thi-phng@student.42.fr>
2023-06-12 13:32:38 +0200

    init react app and vite

---
+++ pong_vite/src/App.tsx
@@ -0,0 +1,47 @@
+
+import Lottie from "lottie-react";
+import animation from "./assets/phone.json";
+
+
+function  App() {
+  return (
+    <>
+      <div className='center'>
+       <div className="body">
+        <h1>Let's go back to the 70s to try the famous</h1>
+        <div className="center">
+          <button > Pong </button>
+          <button> Learn more</button>
+        </div>
+          <p className="read-the-docs">
+            Click on the Pong button to see the magic happen!
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
+
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://www.3dponggame.com/curveball.php" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+
+//       <h1>Let's go back to the 70s to try the famous</h1>
+//       <div className="card">
+//         <button > Pong </button>
+//         {/*<Lottie animationData={animationData} />*/}
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Pong button to see the magic happen!
+//       </p>
+//     </>
+//   )
+// }
+
---
+++ react-app/my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen fex items-center justify-center lg:mx-48 text-white font-poppins">
+      <h1 style={{ color: 'white' }}> Hello </h1>
+    </div>
+  );
+}
+çç
+export default App;
