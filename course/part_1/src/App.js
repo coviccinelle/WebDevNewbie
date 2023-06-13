bd750630076d494aee6a696f490f0e6c6e2602f6
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-06-13 13:30:06 +0200

    part 1 course

---
+++ course/part_1/src/App.js
@@ -0,0 +1,55 @@
+const Header = (props) => {
+	console.log(props)
+  	return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = ({ part1, part2, part3 }) => {
+  return (
+    <div>
+      <Part part={part1.name} exercises={part1.exercises}/>
+      <Part part={part2.name} exercises={part2.exercises}/>
+      <Part part={part3.name} exercises={part3.exercises}/>
+    </div>
+  )
+}
+
+const Total = ({part1, part2, part3}) => {
+  return (
+    <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+	  <Content part1={part1} part2={part2} part3={part3}/>
+	  <Total part1={part1} part2={part2} part3={part3}/>
+    </div>
+  )
+}
+
+
+export default App;
