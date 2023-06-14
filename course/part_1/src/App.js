73f3fbbc4aa9614d7097b2b859bf49c7a7748884
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-06-14 17:35:21 +0200

    p1.c event handler, setCounter

--- course/part_1/src/App.js
+++ course/part_1/src/App.js
@@ -1,30 +1,18 @@
-const Hello = (props) => {
-
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
-
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
+import { useState } from 'react'
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <div>{counter}</div>
+
+      <button onClick={() => setCounter(counter + 1)}> 
+        plus
+      </button>
+	  <button onClick={() => setCounter(0)}>
+	  	zero
+	  </button>
     </div>
   )
 }
