78b4851b65165252b302af131bbecbd7a94e8eb4
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-06-17 11:40:39 +0200

    following the javaScript 1h course on YouTube

---
+++ youtube_course/index.js
@@ -0,0 +1,8 @@
+//performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName + '!');
+}
+//calculating a value
+
+greet('Julien', 'Francais');
+greet('Mary', 'Smith');
