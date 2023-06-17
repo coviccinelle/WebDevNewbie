0abcef2a396feb078d03e07ed98e37c07ef09c27
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-06-17 11:42:32 +0200

    funtions performing a task and a function to calculating a value:

--- youtube_course/index.js
+++ youtube_course/index.js
@@ -3,6 +3,13 @@ function greet(name, lastName) {
     console.log('Hello ' + name + ' ' + lastName + '!');
 }
 //calculating a value
+function square(number) {
+    return number * number;
+}
 
 greet('Julien', 'Francais');
 greet('Mary', 'Smith');
+
+let number = square(2);
+
+console.log(number);
